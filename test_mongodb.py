5ccee1e5d251a27803be256eda730957e414ffd4
Brunobs13 <142010592+Brunobs13@users.noreply.github.com>
2026-02-19 20:52:10 +0100

    Add API entrypoints and MongoDB data ingestion scripts.

---
+++ test_mongodb.py
@@ -0,0 +1,17 @@
+
+import os
+from pymongo.mongo_client import MongoClient
+
+uri = os.getenv("MONGO_DB_URL")
+if not uri:
+    raise ValueError("Set MONGO_DB_URL before running test_mongodb.py")
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
